2e9a36284d13e45dfd9435155c05084ae28d2b87
sudhar <sudhar@proglint.com>
2023-05-15 15:53:52 +0530

    15/05/2023 VNDocumentCameraController

--- ios/Classes/SwiftEdgeDetectionPlugin.swift
+++ ios/Classes/SwiftEdgeDetectionPlugin.swift
@@ -1,9 +1,11 @@
 import Flutter
 import UIKit
 import WeScan
+import Vision
+import VisionKit
 
 public class SwiftEdgeDetectionPlugin: NSObject, FlutterPlugin, UIApplicationDelegate {
-    
+    var _result:FlutterResult?
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "edge_detection", binaryMessenger: registrar.messenger())
         let instance = SwiftEdgeDetectionPlugin()
@@ -13,16 +15,22 @@ public class SwiftEdgeDetectionPlugin: NSObject, FlutterPlugin, UIApplicationDel
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         
+         let args = call.arguments as! Dictionary<String, Any>
+            let saveTo = args["save_to"] as! String
+            let canUseGallery = args["can_use_gallery"] as? Bool ?? false
         if (call.method == "edge_detect")
         {
-            let args = call.arguments as! Dictionary<String, Any>
-            let saveTo = args["save_to"] as! String
-            let canUseGallery = args["can_use_gallery"] as! Bool
+           
             if let viewController = UIApplication.shared.delegate?.window??.rootViewController as? FlutterViewController {
-                let destinationViewController = HomeViewController()
-                destinationViewController.setParams(saveTo: saveTo, canUseGallery: canUseGallery)
-                destinationViewController._result = result
-                viewController.present(destinationViewController,animated: true,completion: nil);
+                self._result = result
+                if #available(iOS 13.0, *) {
+                    let vision = VNDocumentCameraViewController()
+                    vision.delegate = self
+                    viewController.present(vision, animated: true)
+                } else {
+                     
+                }
+              
             }
         }
         if (call.method == "edge_detect_gallery")
@@ -33,8 +41,37 @@ public class SwiftEdgeDetectionPlugin: NSObject, FlutterPlugin, UIApplicationDel
                 let destinationViewController = HomeViewController()
                 destinationViewController.setParams(saveTo: saveTo, canUseGallery: true)
                 destinationViewController._result = result
+                destinationViewController.setParams(saveTo: saveTo, canUseGallery: canUseGallery)
                 destinationViewController.selectPhoto();
             }
         }
     }
+}
+extension SwiftEdgeDetectionPlugin: VNDocumentCameraViewControllerDelegate {
+    
+    @available(iOS 13.0, *)
+    public func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
+        
+       
+        self._result!([])
+        controller.dismiss(animated: true)
+    }
+    
+    @available(iOS 13.0, *)
+    public func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+        
+        var image = [Data]()
+        
+        for test in 0...scan.pageCount - 1 {
+            image.append(scan.imageOfPage(at: test).jpegData(compressionQuality: 1.0)!)
+        }
+       
+        self._result!(image)
+        controller.dismiss(animated: true,completion: nil)
+        
+        
+    }
+    
+    
+
 }
